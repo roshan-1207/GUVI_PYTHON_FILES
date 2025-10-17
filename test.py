82df2e60341216e64eb52e0020c9551909ff9a5b
roshan-1207 <rm8594@srmist.edu.in>
2025-10-17 12:24:00 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,33 @@
+'''def lst(a,b):
+    if b in a:
+        print("the target is present")
+    else:
+        print("the target is not present")'''
+'''def x(n):
+    if n==1:
+        return 1
+    else:
+        return n*x(n-1)'''
+'''def z(x):
+    x=str(x)
+    if x==x[::-1]:
+        print("it is a palindrome")
+    else:
+        print("it is not a palindrome")'''
+'''def a(m):
+    y=list(filter(lambda u:u%2==0,m))
+    z=list(map(lambda v:v**2,y))
+    print(z)'''
+'''x="banana"
+z='a'
+cnt=0
+for c in x:
+    if c==z:
+        cnt +=1
+
+print("count of",z,"=",cnt)'''
+
+
+        
+     
+
